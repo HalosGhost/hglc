dc55cfdec9d0e93ce6585b6f54a2a8a04dc5dc3d
halosghost <samuel.stuewe@gmail.com>
2020-05-06 16:57:27 -0500

    Appropriately handle parsing
    - includes valid parser for Church notation and De Bruijn Index notation

--- src/main.c
+++ src/main.c
@@ -5,43 +5,62 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
-#define PARSERS \
+#define DE_BRUIJN_INDEX_NOTATION \
+    X(Lambda, "lambda", "'\\\\' | \"λ\"") \
+    X(Identifier, "id", "/[0-9]+/") \
+    X(Abstraction, "abs", "<lambda> <term>") \
+    X(Atom, "atom", "'(' <term> ')' | <id>") \
+    X(Application, "app", "<atom> <appL>") \
+    X(Term, "term", "<app> | <abs>") \
+    X(ApplicationL, "appL", "<atom> <appL> | \"\"") \
+    X(Program, "program", "/^/ <abs> /$/")
+
+#define CHURCH_NOTATION \
     X(Lambda, "lambda", "'\\\\' | \"λ\"") \
     X(Dot, "dot", "'.'") \
     X(Identifier, "id", "/[a-z]/") \
-    X(Term, "term", "<app> | <lambda> <id> <dot> <term>") \
-    X(Application, "app", "<app> <atom> | <atom>") \
+    X(Abstraction, "abs", "<lambda> <id> <dot> <term>") \
     X(Atom, "atom", "'(' <term> ')' | <id>") \
-    X(Program, "program", "/^/ <term> /$/")
+    X(Application, "app", "<atom> <appL>") \
+    X(Term, "term", "<app> | <abs>") \
+    X(ApplicationL, "appL", "<atom> <appL> | \"\"") \
+    X(Program, "program", "/^/ <abs> /$/")
 
 #define X(id, name, def) + 1
-static const int PARSER_COUNT = PARSERS;
+static const int PARSER_COUNT = CHURCH_NOTATION;
 #undef X
+
+/**
+ * Todo
+ **
+ * beta()
+ */
 
 signed
 main (void) {
 
     #define X(id, name, def) pcq_parser_t *id = pcq_new(name);
-    PARSERS
+    CHURCH_NOTATION
     #undef X
 
-    pcq_err_t * not_parsed = pcqa_lang(PCQA_LANG_DEFAULT,
+    pcq_err_t * failed_to_parse = pcqa_lang(PCQA_LANG_DEFAULT,
     #define X(id, name, def) name ": " def ";\n"
-        PARSERS
+        CHURCH_NOTATION
     #undef X
     #define X(id, name, def) , id
-        PARSERS,
+        CHURCH_NOTATION,
     #undef X
         NULL
     );
 
-    if ( not_parsed ) {
+    if ( failed_to_parse ) {
         #define X(id, name, def) name ": " def ";\n"
         printf("%s\n",
-            PARSERS
+            CHURCH_NOTATION
         );
         #undef X
-        pcq_err_print(not_parsed);
+
+        pcq_err_print(failed_to_parse);
         return EXIT_FAILURE;
     }
 
@@ -50,7 +69,7 @@ main (void) {
     while ( (input = readline("> ")) ) {
         add_history(input);
 
-        if (pcq_parse("<stdin>", input, Program, &r)) {
+        if ( pcq_parse("<stdin>", input, Program, &r) ) {
             pcq_ast_print(r.output);
             pcq_ast_delete(r.output);
         } else {
@@ -63,7 +82,7 @@ main (void) {
 
     #define X(id, name, def) , id
     pcq_cleanup(PARSER_COUNT
-        PARSERS
+        CHURCH_NOTATION
     );
     #undef X
 
